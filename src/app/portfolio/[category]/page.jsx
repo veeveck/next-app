269ad4251b00d6e6117170e760612f421e05cb99
Vivek <veeveckjeet@gmail.com>
2023-06-12 12:46:36 -0400

    Portfolio done

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import styles from './page.module.css'
+import Button from '@/components/Button/Button'
+import Image from 'next/image'
 
-const Category = () => {
+const Category = ({params}) => {
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+       <h1 className={styles.catTitle}>{params.category}</h1>
+       <div className={styles.item}>
+       <div className={styles.content}>
+        <h1 className={styles.title}>Test</h1>
+        <p className={styles.desc}>Description</p>
+        <Button text="More" url="#"/>
+       </div>
+       <div className={styles.imgContainer}>
+        <Image className={styles.img} fill={true} src="" alt=""/>
+       </div>
+       </div>
+    </div>
   )
 }
 
